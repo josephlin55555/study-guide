a3f992564f3a5ab800f541f8e7183059237cb31f
Joseph Lin <josephlin55555@gmail.com>
2018-06-01 11:46:31 -0400

    [add] methods to binarySearchTree

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -1,1 +1,36 @@
-// Implement Breath-First and Depth-First Search methods
+class Node {
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor(root) {
+        this.root = root;
+    }
+
+    search(value, node = this.root) {
+        if (!node || node.value === value) {
+            return node;
+        } else if (value < node.value) {
+            this.search(value, node.left);
+        } else if (value > node.value) {
+            this.search(value, node.right);
+        }
+    }
+
+    insert(value, node = this.root) {
+        if (!node) {
+            node = new Node(value);
+        } else if (value < node.value) {
+            this.insert(value, node.left);
+        } else if (value > node.value) {
+            this.insert(value, node.right);
+        }
+    }
+
+    delete() {
+
+    }
+
+}
