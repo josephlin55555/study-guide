d1dc12577ab107c7aad6807646d0986fa055d5a9
Joseph Lin <josephlin55555@gmail.com>
2018-06-01 14:21:27 -0400

    [update] delete method for binary search tree

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -19,18 +19,53 @@ class BinarySearchTree {
         }
     }
 
-    insert(value, node = this.root) {
+    insert(value, node = this.root, parentNode) {
         if (!node) {
             node = new Node(value);
+            node.parent = parentNode;
         } else if (value < node.value) {
-            this.insert(value, node.left);
+            this.insert(value, node.left, node);
         } else if (value > node.value) {
-            this.insert(value, node.right);
+            this.insert(value, node.right, node);
         }
     }
 
-    delete() {
-
+    findMinimumNode(node = this.root) {
+        return !node.left ? node : this.findMinimumNode(node.left);
     }
 
+    delete(value, node = this.root) {
+        if (node === null) {
+            return null;
+        } else if (value < node.value) {
+            node.left = this.delete(value, node.left);
+            return node;
+        } else if (value > node.value) {
+            node.right = this.delete(value, node.right);
+            return node;
+        } else {
+            // no children
+            if (!node.left && !node.right) {
+                node = null;
+                return node;
+            }
+
+            // one child
+            if (!node.left) {
+                node = node.right;
+                return node;
+            } else if (!node.right) {
+                node = node.left;
+                return node;
+            }
+
+            // two children
+            const temporaryNode = this.findMinimumNode(node.right);
+            node.value = temporaryNode.value;
+            node.right = this.delete(temporaryNode.value, node.right);
+            return node;
+        }
+    }
 }
+
+
